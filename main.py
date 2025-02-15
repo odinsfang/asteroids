897bebf08f258d90dd39acc43073c1c4c54d6053
odinsfang <odinsfang@gmail.com>
2025-02-15 16:30:27 -0500

    simplified

--- main.py
+++ main.py
@@ -1,21 +1,74 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
+constants.py
+U
+
+main.py
+M
+
+requirements.txt
+U
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
 import pygame
 from constants import *
 
-def main():
 
+def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+import pygame
+from constants import *
+
+
+def main():
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen,(0,0,0))
+
+        screen.fill("black")
         pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
+
